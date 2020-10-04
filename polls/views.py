159b64e4fe4b617d7bf343b91df2d30a3b76ca4d
Shubham Katte <shubhamkatte99@gmail.com>
2020-10-04 14:11:36 +0530

    started

---
+++ polls/views.py
@@ -0,0 +1,25 @@
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+
+def index(request):
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    context={
+        'latest_question_list':latest_question_list
+    }
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at Question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking a the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are votiing on question %s" % question_id)
